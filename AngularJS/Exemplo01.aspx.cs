620c0b2dd9fdba322a0228804a8f007eeef4a608
Eduardo correa dos Santos <eduardocorrea82@hotmail.com>
2017-03-15 12:42:04 -0300

    15/03/17 12:41

---
+++ AngularJS/Exemplo01.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
+namespace AngularJS
+{
+    public partial class Exemplo01 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        [WebMethod]
+        public static string TestAjax(string teste)
+        {
+            string json = JsonConvert.SerializeObject(new {Nome= "Eduardo",SobreNome="Correa" });
+            return json;
+        }        
+    }
+}
